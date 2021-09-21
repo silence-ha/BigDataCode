fafb8e220d9baaf8f3d2e79f6b4565b432a18a5b
silence <1399912833@qq.com>
2021-09-21 20:53:23 +0800

    flink

--- src/main/java/com/silence/marketanalysis/AdStatisticsByProvince.java
+++ src/main/java/com/silence/marketanalysis/AdStatisticsByProvince.java
@@ -4,15 +4,23 @@ import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 
 import java.time.Duration;
 
@@ -37,7 +45,50 @@ public class AdStatisticsByProvince {
                         return adClickEvent.getTimestamp() * 1000l;
                     }
                 }));
-        KeyedStream<AdClickEvent, String> keyedDs = waterDs.keyBy(new KeySelector<AdClickEvent, String>() {
+
+        SingleOutputStreamOperator<AdClickEvent> processDs = waterDs.keyBy("userId", "adId")
+                .process(new KeyedProcessFunction<Tuple, AdClickEvent, AdClickEvent>() {
+                    ValueState<Long> val;
+                    ValueState<Boolean> isSentState;
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        val = getRuntimeContext().getState(new ValueStateDescriptor<Long>("val", Long.class,0l));
+                        isSentState = getRuntimeContext().getState(new
+                                ValueStateDescriptor<Boolean>("is-sent", Boolean.class, false));
+                    }
+
+                    @Override
+                    public void processElement(AdClickEvent adClickEvent, Context context, Collector<AdClickEvent> collector) throws Exception {
+                        Long c = val.value();
+                        if (c == 0) {
+                            val.update(c + 1);
+                            Long ts = adClickEvent.getTimestamp() * 1000 + 60 * 60 * 1000;
+                            context.timerService().registerEventTimeTimer(ts);
+                            collector.collect(adClickEvent);
+                        } else if (c < 100) {
+                            val.update(c + 1);
+                            collector.collect(adClickEvent);
+                        } else {
+                            if (!isSentState.value()) {
+                                isSentState.update(true);
+                                context.output(new OutputTag<AdClickEvent>("blacklist") {
+                                }, adClickEvent);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onTimer(long timestamp, OnTimerContext ctx, Collector<AdClickEvent> out) throws Exception {
+                        val.clear();
+                        isSentState.clear();
+                    }
+                });
+        processDs.getSideOutput(new OutputTag<AdClickEvent>("blacklist") {})
+        .print("black------");
+
+
+        KeyedStream<AdClickEvent, String> keyedDs = processDs.keyBy(new KeySelector<AdClickEvent, String>() {
 
             @Override
             public String getKey(AdClickEvent adClickEvent) throws Exception {
